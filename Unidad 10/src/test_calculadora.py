60a04c86b6fcfb8b724a5115429560494a1a4cc1
Juan Ignacio Hernández <hernandezjuanvt@gmail.com>
2022-11-18 11:17:36 -0300

    Fix: Calculator functions improved. Better Unit tests.

--- Unidad 10/src/test_calculadora.py
+++ Unidad 10/src/test_calculadora.py
@@ -5,33 +5,31 @@ import calculadora
 class TestCalculadora(unittest.TestCase):
     def test_sumar(self):
         self.assertEqual(calculadora.sumar(2, 3), 5)
-        self.assertEqual(calculadora.sumar(6, 13), 19)
-        self.assertEqual(calculadora.sumar(-5, -4), -9)
-        with self.assertRaises(TypeError):
-            calculadora.sumar(5, 'hello')
+        self.assertEqual(calculadora.sumar(-4, 8, -2), 2)
+        self.assertEqual(calculadora.sumar(3, -15, 8, 0, -3), -7)
+        self.assertEqual(calculadora.sumar(1, [3, 2], 9), None)
+        self.assertEqual(calculadora.sumar(3, 6, '10'), None)
 
     def test_restar(self):
-        self.assertEqual(calculadora.restar(4, 1), 3)
-        self.assertEqual(calculadora.restar(3, -2), 5)
-        self.assertEqual(calculadora.restar(4, 6), -2)
-        with self.assertRaises(TypeError):
-            calculadora.restar(8, 'asd')
+        self.assertEqual(calculadora.restar(6, 2), 4)
+        self.assertEqual(calculadora.restar(8, -5, -4), 17)
+        self.assertEqual(calculadora.restar(-5, 11, -4, 1, -3), -10)
+        self.assertEqual(calculadora.restar(4, {-4, 1}, -7), None)
+        self.assertEqual(calculadora.restar({3: 2}, 4, '-8'), None)
 
     def test_multiplicar(self):
         self.assertEqual(calculadora.multiplicar(0, 6), 0)
         self.assertEqual(calculadora.multiplicar(1, 3), 3)
-        self.assertEqual(calculadora.multiplicar(7, -8), -56)
-        with self.assertRaises(TypeError):
-            calculadora.multiplicar(1, 'asd')
+        self.assertEqual(calculadora.multiplicar(7, -8, 6, 2), -672)
+        self.assertEqual(calculadora.multiplicar(1, '3', 4, -2), None)
+        self.assertEqual(calculadora.multiplicar(7, -8, 0, -1, {2, -4}), None)
 
     def test_dividir(self):
         self.assertEqual(calculadora.dividir(5, 5), 1)
-        self.assertEqual(calculadora.dividir(12, 4), 3)
-        with self.assertRaises(TypeError):
-            calculadora.dividir(3, 'bye')
-        # self.assertEqual(calculadora.dividir(3, 0), None)
-        with self.assertRaises(ZeroDivisionError):
-            calculadora.dividir(6, 0)
+        self.assertEqual(calculadora.dividir(-12, 4), -3)
+        self.assertEqual(calculadora.dividir(128, 2, 2, 2, 2, 2), 4)
+        self.assertEqual(calculadora.dividir(12, 0, -2), None)
+        self.assertEqual(calculadora.dividir(5, '4', -2, [1, 2]), None)
 
 
 if __name__ == "__main__":
